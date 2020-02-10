658510e21af22c9373427ae4244f3b49470abf0d
Ruize <rz376775@dal.ca>
2020-02-10 18:10:31 -0400

    initial bottom navigation bar completed

--- SafeWorld/app/src/main/java/com/safeworld/MainActivity.java
+++ SafeWorld/app/src/main/java/com/safeworld/MainActivity.java
@@ -1,14 +1,58 @@
 package com.safeworld;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                new NewsFragment()).commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_news:
+                            openFragment(new NewsFragment());
+                            break;
+                        case R.id.navigation_courses:
+                            openFragment(new CoursesFragment());
+                            break;
+                        case R.id.navigation_posts:
+                            openFragment(new PostsFragment());
+                            break;
+                        case R.id.navigation_profile:
+                            openFragment(new ProfileFragment());
+                            break;
+                        case R.id.navigation_upgrade:
+                            openFragment(new UpgradeFragment());
+                            break;
+                    }
+                    return true;
+                }
+            };
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ SafeWorld/app/src/main/java/com/safeworld/UpgradeFragment.java
@@ -0,0 +1,20 @@
+package com.safeworld;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class UpgradeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_upgrade, container, false);
+        return root;
+    }
+}
