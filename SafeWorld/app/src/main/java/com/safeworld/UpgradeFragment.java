74e392892a4e53f8d130369ecb1c3cb8af3bedcf
Ruize <rz376775@dal.ca>
2020-03-23 17:53:14 -0300

    upgrade initial done

--- SafeWorld/app/src/main/java/com/safeworld/UpgradeFragment.java
+++ SafeWorld/app/src/main/java/com/safeworld/UpgradeFragment.java
@@ -5,16 +5,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.safeworld.upgrade.CardItem;
+import com.safeworld.upgrade.CardPagerAdapter;
+import com.safeworld.upgrade.ShadowTransformer;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 public class UpgradeFragment extends Fragment {
 
+    private ViewPager mViewPager;
+    private CardPagerAdapter mCardAdapter;
+    private ShadowTransformer mCardShadowTransformer;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View root = inflater.inflate(R.layout.fragment_upgrade, container, false);
+        mViewPager = root.findViewById(R.id.view_pager);
+
+        mCardAdapter = new CardPagerAdapter(getContext());
+        mCardAdapter.addCardItem(new CardItem(R.string.title_1, R.string.text_1, R.string.subtext_1));
+        mCardAdapter.addCardItem(new CardItem(R.string.title_2, R.string.text_2, R.string.subtext_2));
+//        mCardAdapter.addCardItem(new CardItem(R.string.title_3, R.string.text_3, R.string.subtext_3));
+
+        mCardShadowTransformer = new ShadowTransformer(mViewPager, mCardAdapter);
+        mViewPager.setAdapter(mCardAdapter);
+        mViewPager.setPageTransformer(false, mCardShadowTransformer);
+        mViewPager.setOffscreenPageLimit(3);
+
+        mCardShadowTransformer.enableScaling(true);
         return root;
     }
 }
